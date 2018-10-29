2938727656276cb70dfbee6f998e232e175781e9
JohnRFoster <fosterj@bu.edu>
2018-10-29 18:15:41 -0400

    add example and export

--- R/update_fosteR.R
+++ R/update_fosteR.R
@@ -1,3 +1,14 @@
+##---------------------------------------------------------------------------------##
+##                                Automatic update                                 ##
+##---------------------------------------------------------------------------------##
+
+#' Automatic update function
+#'
+#' @export
+#' @examples update_fosteR()
+
+
+
 update_fosteR <- function(){
   library(devtools)
   install_github("JohnRFoster/fosteR")
