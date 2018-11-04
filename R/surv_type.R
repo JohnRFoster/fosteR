2128861b9ca0638440b5ea60aff4a9214d3635dc
JohnRFoster <fosterj@bu.edu>
2018-11-03 23:01:35 -0400

    change the way data is subset

--- R/surv_type.R
+++ R/surv_type.R
@@ -9,21 +9,19 @@
 #' the data to be fed to JAGS
 #'
 #' @param file Path to CSV
-#' @param season "winter" or "summer"
-#' @param winter.start date when winter season starts "yyyy-mm-dd"
-#' @param winter.end date when winter season ends "yyyy-mm-dd"
-#' @param life.stage "Larvae" or "Nymph"
-#' @param flat.fed "Flat" or "Fed"
+#' @param subset one of "Fed Larvae", "Flat Larvae", "Fed Nymph", "Flat Nymph", "Overwintering Nymph"
+#' @param season default = NULL, must set to "winter or "summer for larvae
 #' @export
-#' @examples surv.type(file, "summer", "2017-10-01", "2018-05-31", "Nymph", "Flat")
+#' @examples surv.type(file, "Fed Nymph")
 
 
-surv_type <- function(file, season, winter.start, winter.end, life.stage, flat.fed){
+surv_type <- function(file, subset, season = NULL){
 
   rawdata <- read.csv(file)
   dat <- rawdata
   dat$Bin <- 1:nrow(dat) # create column that is just row numbers
 
+  dat$Deployment_Name <- as.character(dat$Deployment_Name)
   dat$Site <- as.numeric(dat$Site)  # convert site to numeric IDs
 
   # convert from factor to date
@@ -33,26 +31,19 @@ surv_type <- function(file, season, winter.start, winter.end, life.stage, flat.f
   start.date <- dat$Date_Deployed   # extract start date
   end.date <- dat$Date_Retrieved    # extract end date
 
-  # rows the represent over-winter experiments
-  over.winter <- which(end.date <= as.Date(winter.end) &
-                         start.date >= as.Date(winter.start))
-
-  # rows the represent over-summer experiments
-  over.summer <- which(end.date < as.Date(winter.start) |
-                         start.date > as.Date(winter.end))
-
-  ## over winter or within-season experiments?
-  if(season == "winter"){
-    dat <- dat[over.winter,]   # all over winter data
-  }
-  if(season == "summer"){
-    dat <- dat[over.summer,]   # all summer data
-  }
-
-  dat <- subset(dat, Stage == life.stage)       # subset life stage
-  dat <- subset(dat, Flat_Fed == flat.fed)      # subset flat or fed ticks
   dat <- subset(dat, Animal_Disturbance == "N") # subset no animal disutbance
   dat <- subset(dat, Tick_Source == "NY")       # subset only ticks from NY
+  dat <- subset(dat, !is.na(dat$N_Recovered))   # subset ticks with conflicting info in N_Recovered
+  dat <- subset(dat, Deployment_Name == subset) # subset data to use
+
+  # parse larve into winter and summer subset
+  if(dat$Deployment_Name == "Flat Larvae" || dat$Deployment_Name == "Fed Larvae"){
+    if(season == "winter"){
+      dat <- subset(dat, Date_Retrieved >= "2018-01-01")
+    } else {
+      dat <- subset(dat, Date_Retrieved < "2018-01-01")
+    }
+  }
 
   # sequence of all days within range of subsetted data
   days.seq <- seq.Date(min(dat$Date_Deployed),
@@ -61,17 +52,20 @@ surv_type <- function(file, season, winter.start, winter.end, life.stage, flat.f
   # Number of days in soil
   N_Days <- as.numeric(difftime(dat$Date_Retrieved, dat$Date_Deployed))
 
-  # JAGS data for flat ticks
-  if(flat.fed == "Flat"){
+  if(dat$Deployment_Name == "Flat Larvae" ||
+     dat$Deployment_Name == "Flat Nymph" ||
+     dat$Deployment_Name == "Overwintering Nymph"){
+
+    # JAGS data for flat ticks
     data = list(y = cbind(dat$N_Deployed, dat$N_Recovered),
                 N_days = N_Days,
                 site.index = dat$Site,
                 N = nrow(dat),
                 sites = unique(dat$Site),
                 N_site = length(unique(dat$Site)))
-  }
-  # JAGS data for fed ticks (includes number successfully molted)
-  if(flat.fed == "Fed"){
+  } else {
+
+    # JAGS data for fed ticks (includes number successfully molted)
     data = list(y = cbind(dat$N_Deployed, dat$N_Survived),
                 N_days = N_Days,
                 site.index = dat$Site,
@@ -81,6 +75,7 @@ surv_type <- function(file, season, winter.start, winter.end, life.stage, flat.f
   }
   return(data)
 }
+
 
 
 
--- R/surv_type_NOAA.R
+++ R/surv_type_NOAA.R
@@ -10,20 +10,20 @@
 #' the data to be fed to JAGS
 #'
 #' @param file Path to CSV
-#' @param season "winter" or "summer"
-#' @param winter.start date when winter season starts "yyyy-mm-dd"
-#' @param winter.end date when winter season ends "yyyy-mm-dd"
-#' @param life.stage "Larvae" or "Nymph"
-#' @param flat.fed "Flat" or "Fed"
+#' @param subset one of "Fed Larvae", "Flat Larvae", "Fed Nymph", "Flat Nymph", "Overwintering Nymph"
+#' @param season default = NULL, must set to "winter or "summer for larvae
 #' @export
-#' @examples surv.type(file, "summer", "2017-10-01", "2018-05-31", "Nymph", "Flat")
+#' @examples surv.type(file, "Fed Nymph")
 
 
-surv_type_NOAA <- function(file, season, winter.start, winter.end, life.stage, flat.fed){
+surv_type_NOAA <- function(file, subset, season = NULL){
 
   rawdata <- read.csv(file)
   dat <- rawdata
   dat$Bin <- 1:nrow(dat) # create column that is just row numbers
+
+  dat$Deployment_Name <- as.character(dat$Deployment_Name)
+  dat$Site <- as.numeric(dat$Site)  # convert site to numeric IDs
 
   # convert from factor to date
   dat$Date_Deployed <- as.Date(as.character(dat$Date_Deployed), "%m/%d/%Y")
@@ -32,34 +32,26 @@ surv_type_NOAA <- function(file, season, winter.start, winter.end, life.stage, f
   start.date <- dat$Date_Deployed   # extract start date
   end.date <- dat$Date_Retrieved    # extract end date
 
-  # rows the represent over-winter experiments
-  over.winter <- which(end.date <= as.Date(winter.end) &
-                         start.date >= as.Date(winter.start))
-
-  # rows the represent over-summer experiments
-  over.summer <- which(end.date < as.Date(winter.start) |
-                         start.date > as.Date(winter.end))
-
-  ## over winter or within-season experiments?
-  if(season == "winter"){
-    dat <- dat[over.winter,]   # all over winter data
-  }
-  if(season == "summer"){
-    dat <- dat[over.summer,]   # all summer data
-  }
-
-  dat <- subset(dat, Stage == life.stage)       # subset life stage
-  dat <- subset(dat, Flat_Fed == flat.fed)      # subset flat or fed ticks
   dat <- subset(dat, Animal_Disturbance == "N") # subset no animal disutbance
   dat <- subset(dat, Tick_Source == "NY")       # subset only ticks from NY
   dat <- subset(dat, !is.na(dat$N_Recovered))   # subset ticks with conflicting info in N_Recovered
+  dat <- subset(dat, Deployment_Name == subset) # subset data to use
 
-  # Number of days in soil
-  N_Days <- as.numeric(difftime(dat$Date_Retrieved, dat$Date_Deployed))
+  # parse larve into winter and summer subset
+  if(dat$Deployment_Name == "Flat Larvae" || dat$Deployment_Name == "Fed Larvae"){
+    if(season == "winter"){
+      dat <- subset(dat, Date_Retrieved >= "2018-01-01")
+    } else {
+      dat <- subset(dat, Date_Retrieved < "2018-01-01")
+    }
+  }
 
   # sequence of all days within range of subsetted data
   days.seq <- seq.Date(min(dat$Date_Deployed),
                        max(dat$Date_Retrieved), 1)
+
+  # Number of days in soil
+  N_Days <- as.numeric(difftime(dat$Date_Retrieved, dat$Date_Deployed))
 
   # read in NOAA data
   met.cl <- read.csv("CampLejune_NOAA.csv")
@@ -117,16 +109,17 @@ surv_type_NOAA <- function(file, season, winter.start, winter.end, life.stage, f
 
   # indexing variable for when in sequence core was deployed
   start.index <- vector()
-  end.index <- vector()
   for(i in 1:nrow(dat)){
     start.index[i] <- which(dat$Date_Deployed[i] == days.seq)
-    end.index[i] <- which(dat$Date_Retrieved[i] == days.seq)
   }
 
   dat$Site <- as.numeric(dat$Site)  # convert site to numeric IDs
 
-  # JAGS data for flat ticks
-  if(flat.fed == "Flat"){
+  if(dat$Deployment_Name == "Flat Larvae" ||
+     dat$Deployment_Name == "Flat Nymph" ||
+     dat$Deployment_Name == "Overwintering Nymph"){
+
+    # JAGS data for flat ticks
     data = list(y = cbind(dat$N_Deployed, dat$N_Recovered),
                 N_days = N_Days,
                 site.index = dat$Site,
@@ -136,9 +129,9 @@ surv_type_NOAA <- function(file, season, winter.start, winter.end, life.stage, f
                 sites = unique(dat$Site),
                 N_site = length(unique(dat$Site)),
                 met = met.x)
-  }
-  # JAGS data for fed ticks (includes number successfully molted)
-  if(flat.fed == "Fed"){
+  } else {
+
+    # JAGS data for fed ticks (includes number successfully molted)
     data = list(y = cbind(dat$N_Deployed, dat$N_Survived),
                 N_days = N_Days,
                 site.index = dat$Site,
@@ -151,4 +144,5 @@ surv_type_NOAA <- function(file, season, winter.start, winter.end, life.stage, f
   }
   return(data)
 }
+
 
